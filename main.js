f0ad10021aa3ba7bfd553f6a238426f184e85848
rolindaz <rolinda.zamboni@gmail.com>
2025-06-26 12:46:12 +0200

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log('hey roli');
+
+import chalk from 'chalk';
+
+console.log(chalk.blue('Rolinda è triste'));
+console.log(chalk.bgGreen("I'm in love"));
+
+
+console.log(chalk.bgRed(process.env.super_secret_key));
+
+console.log(chalk.bgMagenta(process.env.my_loved_ones));
+
+console.log(chalk.bgYellow(process.env.my_cats));
+
